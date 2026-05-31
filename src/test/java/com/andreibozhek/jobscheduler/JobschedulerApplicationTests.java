5964d5c2c92ab3b9a717c00369c91227f3e59750
Andrei <2bozhek@gmail.com>
2026-06-01 00:30:52 +0900

    test: add PostgreSQL integration test setup

--- src/test/java/com/andreibozhek/jobscheduler/JobschedulerApplicationTests.java
+++ src/test/java/com/andreibozhek/jobscheduler/JobschedulerApplicationTests.java
@@ -1,16 +1,10 @@
 package com.andreibozhek.jobscheduler;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-		properties = "worker.enabled=false"
-)
-class JobschedulerApplicationTests {
+class JobschedulerApplicationTests extends IntegrationTestBase {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
