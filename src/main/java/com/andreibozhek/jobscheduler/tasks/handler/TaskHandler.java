78210c7e258d548475ef4fc895e630e17044ea31
Andrei <2bozhek@gmail.com>
2026-07-22 15:58:15 +0900

    refactor: add task handler interface

---
+++ src/main/java/com/andreibozhek/jobscheduler/tasks/handler/TaskHandler.java
@@ -0,0 +1,13 @@
+package com.andreibozhek.jobscheduler.tasks.handler;
+
+import com.andreibozhek.jobscheduler.tasks.domain.Task;
+
+public class TaskHandler {
+    String type() {
+        return "";
+    }
+
+    void handle(Task task) {
+
+    }
+}
