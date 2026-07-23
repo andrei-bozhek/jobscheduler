038b25899d2de9445165c23b1006ce780f25fad4
Andrei <2bozhek@gmail.com>
2026-07-23 16:58:48 +0900

    test: configure PostgreSQL test container

--- src/test/java/com/andreibozhek/jobscheduler/IntegrationTestBase.java
+++ src/test/java/com/andreibozhek/jobscheduler/IntegrationTestBase.java
@@ -3,26 +3,33 @@ package com.andreibozhek.jobscheduler;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         properties = "worker.enabled=false"
 )
 public abstract class IntegrationTestBase {
 
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres =
+    static final PostgreSQLContainer<?> postgres =
             new PostgreSQLContainer<>("postgres:16");
 
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureDatasource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    protected JdbcTemplate jdbcTemplate;
 
     @BeforeEach
     void cleanDatabase() {
