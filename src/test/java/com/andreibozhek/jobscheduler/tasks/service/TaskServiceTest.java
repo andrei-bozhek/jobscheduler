9e4cd011d272af9075ab908baddd2157627d7b00
Andrei <2bozhek@gmail.com>
2026-07-23 17:59:20 +0900

    test: create task with default max attempts

---
+++ src/test/java/com/andreibozhek/jobscheduler/tasks/service/TaskServiceTest.java
@@ -0,0 +1,35 @@
+package com.andreibozhek.jobscheduler.tasks.service;
+
+import com.andreibozhek.jobscheduler.IntegrationTestBase;
+import com.andreibozhek.jobscheduler.tasks.api.CreateTaskRequest;
+import com.andreibozhek.jobscheduler.tasks.domain.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.OffsetDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TaskServiceTest extends IntegrationTestBase {
+
+    @Autowired
+    TaskService service;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void createTaskUsesDefaultMaxAttempts() {
+        CreateTaskRequest request = new CreateTaskRequest(
+                "echo",
+                objectMapper.createObjectNode().put("message", "hello"),
+                OffsetDateTime.now().plusMinutes(1),
+                null
+        );
+
+        Task task = service.create(request);
+
+        assertThat(task.maxAttempts()).isEqualTo(3);
+    }
+}
